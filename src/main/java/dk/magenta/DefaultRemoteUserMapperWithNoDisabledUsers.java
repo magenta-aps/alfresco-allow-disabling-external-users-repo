710ac3f62ebb4a3135de1c939baa7488f5eb2e61
Seth Yastrov <syastrov@gmail.com>
2016-06-09 13:30:43 +0200

    Initial commit.

---
+++ src/main/java/dk/magenta/DefaultRemoteUserMapperWithNoDisabledUsers.java
@@ -0,0 +1,46 @@
+package dk.magenta;
+
+import net.sf.acegisecurity.UserDetails;
+import net.sf.acegisecurity.providers.dao.AuthenticationDao;
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.security.authentication.MutableAuthenticationDao;
+import org.alfresco.repo.security.authentication.external.DefaultRemoteUserMapper;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.security.PersonService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by seth on 09/06/16.
+ */
+public class DefaultRemoteUserMapperWithNoDisabledUsers extends DefaultRemoteUserMapper {
+    private MutableAuthenticationDao authenticationDao;
+
+    @Override
+    public String getRemoteUser(HttpServletRequest request) {
+        String remoteUser = super.getRemoteUser(request);
+        if (isUserEnabled(remoteUser)) {
+            return remoteUser;
+        } else {
+            // Don't allow disabled users to login.
+            return null;
+        }
+    }
+
+    public boolean isUserEnabled(final String userName) {
+        UserDetails userDetails = authenticationDao.loadUserByUsername(userName);
+        if (userDetails == null) {
+            System.out.println("Person doesn't exist");
+            return false;
+        }
+
+        return userDetails.isEnabled();
+    }
+
+    public void setAuthenticationDao(MutableAuthenticationDao authenticationDao) {
+        this.authenticationDao = authenticationDao;
+    }
+}
